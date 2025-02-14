03f2f51474b1d02365a6c7d34a6d042264e8280f
trank <Kevin.Tran@pointclickcare.com>
2025-02-14 17:34:21 -0500

    Example to show that we can handle many Twilio accounts in the same app.

---
+++ src/main/java/com/mycompany/myapp/twilo/api/TwilioConfig.java
@@ -0,0 +1,31 @@
+package com.mycompany.myapp.twilo.api;
+
+public class TwilioConfig {
+    private String accountSid;
+    private String apiKey;
+    private String apiSecret;
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+}
